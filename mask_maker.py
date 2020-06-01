f7f42dfd0d6df59f3762dfcf7dea14c4858b41d8
John Orlowski-Scherer <jorlo@nia-login05.scinet.local>
2020-06-01 13:39:17 -0400

    testing mask making

--- mask_maker.py
+++ mask_maker.py
@@ -1,6 +1,7 @@
 from pixell import enmap,utils
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy.table import QTable
 import os,sys
 import urllib.request
 
@@ -11,3 +12,21 @@ def mask(imap, ra, dec, width = 0.5, apod_pix = 4):
 	stamp = imap.submap(box)
 	taper = enmap.apod(stamp*0+1,apod_pix)
 	return taper
+
+mappath = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_boss/'
+
+t = QTable.read('madcows/AdvACT_S18Clusters_v1.0-beta.fits')
+
+boss_mask = enmap.read_map(mappath + "tilec_mask.fits")
+boss_map = enmap.read_map(mappath + 'tilec_single_tile_boss_comptony_map_v1.2.0_joint.fits')
+
+ra_temp = t['RADeg']
+dec_temp = t['decDeg']
+ra, dec = np.array(ra_temp), np.array(dec_temp)
+
+test_taper = mask(boss_map, ra[0], dec[0])
+
+plt.imshow(test_taper)
+plt.colorbar()
+plt.savefig("test_taper.png")
+plt.close()

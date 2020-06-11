c2a0ae011cd9349a5bfdafd2b9bfa8af39bc43ed
John Orlowski-Scherer <jorlo@nia-login05.scinet.local>
2020-06-11 13:56:05 -0400

    Making freq stamps for Luca

--- mask_maker.py
+++ mask_maker.py
@@ -6,16 +6,16 @@ import os,sys
 import urllib.request
 from astropy.io import fits
 
-def mask(imap, s14map, ra, dec, name, width = 0.5, apod_pix = 4):
+def mask(imap, ra, dec, name, width = 0.5, apod_pix = 4):
 	ra, dec = np.deg2rad([ra,dec])
 	width = np.deg2rad(width)
 	box = [[dec-width/2.,ra-width/2.],[dec+width/2.,ra+width/2.]]
 	stamp = imap.submap(box)
 	taper = enmap.apod(stamp*0+1,apod_pix)
 	name = name.replace(' ', '-').lower()
-	enmap.write_map('{}.fits'.format(name), taper)
-	s14_stamp = s14map.submap(box)
-	enmap.wrate('s14_{}__pa1_f150_nohwp_night_3pass_4way_set0_ivar.fits'.format(name), s14_stamp)
+	enmap.write_map('./padded_v1/adv{}.fits'.format(name), taper)
+	#s14_stamp = s14map.submap(box)
+	#enmap.write('./padded_v0/s14_{}__pa1_f150_nohwp_night_3pass_4way_set0_ivar.fits'.format(name), s14_stamp)
 	return taper
 
 mappath = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_boss/'
@@ -24,14 +24,14 @@ t = QTable.read('madcows/AdvACT_S18Clusters_v1.0-beta.fits')
 
 boss_mask = enmap.read_map(mappath + "tilec_mask.fits")
 boss_map = enmap.read_map(mappath + 'tilec_single_tile_boss_comptony_map_v1.2.0_joint.fits')
-s14 = enmap.read_map('/home/r/rbond/sigurdkn/project/actpol/maps/mr3f_20190502/release/s14_boss_pa1_f150_nohwp_night_3pass_4way_set0_ivar.fits')
+#s14 = enmap.read_map('/home/r/rbond/sigurdkn/project/actpol/maps/mr3f_20190502/release/s14_boss_pa1_f150_nohwp_night_3pass_4way_set0_ivar.fits')
 
 ra_temp = t['RADeg']
 dec_temp = t['decDeg']
 names = t['name']
 ra, dec = np.array(ra_temp), np.array(dec_temp)
 
-test_taper = mask(boss_map, ra[0], dec[0], names[0])
+test_taper = mask(boss_map, ra[3], dec[3], names[3])
 
 #plt.imshow(test_taper)
 #plt.colorbar()
--- stacker.py
+++ stacker.py
@@ -25,6 +25,7 @@ def stack(ras, decs, map1, mask1, map2 = None, mask2 = None, width = 20.):
 		#If a second map is to be checked, repeat above with second map
 		maskstamp = mask2.submap(box)
 		if np.any(maskstamp[0]):
+                        print(i)
                         stamp = reproject.postage_stamp(map2, ras[i], decs[i], width, 0.5)
                         if stamp is None: continue
                         stack += stamp[0]
@@ -34,42 +35,66 @@ def stack(ras, decs, map1, mask1, map2 = None, mask2 = None, width = 20.):
 	print(divisor)
 	return stack
 
-mappath = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_boss/'
+#Stack on S18 cluster catalog
 
-t = QTable.read('madcows/AdvACT_S18Clusters_v1.0-beta.fits')
+"""
+if 0:
+	mappath = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_boss/'
 
-boss_mask = enmap.read_map(mappath + "tilec_mask.fits")
-boss_map = enmap.read_map(mappath + 'tilec_single_tile_boss_comptony_map_v1.2.0_joint.fits')
+	t = QTable.read('madcows/AdvACT_S18Clusters_v1.0-beta.fits')
 
-ra_temp = t['RADeg']
-dec_temp = t['decDeg']
-ra, dec = np.array(ra_temp), np.array(dec_temp)
+	boss_mask = enmap.read_map(mappath + "tilec_mask.fits")
+	boss_map = enmap.read_map(mappath + 'tilec_single_tile_boss_comptony_map_v1.2.0_joint.fits')
 
-boss_stack = stack(ra, dec, boss_map, boss_mask)
-plt.imshow(boss_stack)
-plt.colorbar()
-plt.savefig("boss_stack_act_y.png")
-plt.close()
+	ra_temp = t['RADeg']
+	dec_temp = t['decDeg']
+	ra, dec = np.array(ra_temp), np.array(dec_temp)
 
-
-#plots = enplot.plot(enmap.upgrade(boss_stack,5),grid=False, colorbar=True,color='gray')
-#enplot.write("boss_stack_act_y",plots)
-
-d56_path = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_deep56/'
-
-d56_mask = enmap.read_map(d56_path + "tilec_mask.fits")
-d56_map = enmap.read_map(d56_path + 'tilec_single_tile_deep56_comptony_map_v1.2.0_joint.fits')
+	boss_stack = stack(ra, dec, boss_map, boss_mask)
+	plt.imshow(boss_stack)
+	plt.colorbar()
+	plt.savefig("boss_stack_act_y.png")
+	plt.close()
 
 
-d56_stack = stack(ra, dec, d56_map, d56_mask)
-plt.imshow(d56_stack)
-plt.colorbar()
-plt.savefig("d56_stack_act_y.png")
-plt.close()
+	#plots = enplot.plot(enmap.upgrade(boss_stack,5),grid=False, colorbar=True,color='gray')
+	#enplot.write("boss_stack_act_y",plots)
 
-comb_stack = stack(ra, dec, boss_map, boss_mask, d56_map, d56_mask)
-plt.imshow(comb_stack)
-plt.colorbar()
-plt.savefig("comb_stack_act_y.png")
-plt.close()
+	d56_path = '/scratch/r/rbond/msyriac/data/depot/tilec/v1.2.0_20200324/map_v1.2.0_joint_deep56/'
 
+	d56_mask = enmap.read_map(d56_path + "tilec_mask.fits")
+	d56_map = enmap.read_map(d56_path + 'tilec_single_tile_deep56_comptony_map_v1.2.0_joint.fits')
+
+
+	d56_stack = stack(ra, dec, d56_map, d56_mask)
+	plt.imshow(d56_stack)
+	plt.colorbar()
+	plt.savefig("d56_stack_act_y.png")
+	plt.close()
+
+	comb_stack = stack(ra, dec, boss_map, boss_mask, d56_map, d56_mask)
+	plt.imshow(comb_stack)
+	plt.colorbar()
+	plt.savefig("comb_stack_act_y.png")
+	plt.close()
+"""
+
+mappath = '/home/r/rbond/sigurdkn/project/actpol/map_coadd/20200228/release/'
+	
+freqs = ['f090', 'f150', 'f220']
+
+for freq in freqs:
+	name = 'act_planck_s08_s18_cmb_{}_night_map.fits'.format(freq)
+	print(name)		
+	imap = enmap.read_map(mappath + name)
+	
+	ra, dec = np.deg2rad([20.,1.])
+	width = np.deg2rad(0.5)
+
+	box = [[dec-width/2.,ra-width/2.],[dec+width/2.,ra+width/2.]]
+	stamp = imap.submap(box)
+
+	plots = enplot.plot(stamp,range=300,mask=0)
+	enplot.write(name,plots)
+
+	enmap.write_map('./padded_v1/{}.fits'.format(name), stamp)

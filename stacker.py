8da99de2d21d49807662038c0a4d82e42610bf99
John Orlowski-Scherer <jorlo@nia-login01.scinet.local>
2020-08-12 15:14:49 -0400

    Postage stamps on negative y0 points in freq maps

--- stacker.py
+++ stacker.py
@@ -6,6 +6,20 @@ import os,sys
 import urllib.request
 from astropy.table import QTable
 import astropy.units as u
+import yaml
+from astropy.io import fits
+import matplotlib.patheffects as path_effects
+
+def tileFinder(ra, dec, data):
+    #Given an RA and Dec in deg, find the S18d tile containing that RA and Dec
+    for i, tile in enumerate(data):
+        box = tile['RADecSection']
+        if box[0] >= box[1]:
+            if (360 >= ra >= box[0] or 0 <= ra <box[1]) and box[2]<=dec<= box[3]:
+                return tile['tileName']
+        if box[0]<=ra<=box[1] and box[2]<=dec<= box[3]:
+            return tile['tileName']
+    return None
 
 def stack(ras, decs, map1, mask1, map2 = None, mask2 = None, width = 20.):
 	stack = 0
@@ -38,29 +52,119 @@ def freqStamp(ra, dec, fmap, name, width = 0.5, write = True):
     
     stamp = reproject.postage_stamp(fmap, ra, dec, width*60, 0.5)
     if write:
-        tempdec, tempra = np.deg2rad([dec, ra])
-        tempwid = np.deg2rad(width)
-        box = [[tempdec-tempwid,tempra-tempwid],[tempdec+tempwid,tempra+tempwid]]
- 
-        stampgeo = fmap.submap(box)
-        print(stampgeo.wcs)
-        print(stamp.wcs)
-        stamp.wcs = stampgeo.wcs
-        print(stamp.wcs)
-        #plt.imshow(stamp[0])
-        #plt.show()
-        #Return map
+        stamp.wcs.wcs.crval = [ra,dec]
         enmap.write_map('./for_tony/{}.fits'.format(name), stamp)
     return stamp
 
-path = '/home/r/rbond/sigurdkn/project/actpol/map_coadd/20200228/release2/'
-map220 = enmap.read_map(path + 'act_planck_s08_s18_cmb_f220_night_map.fits')
-map150 = enmap.read_map(path + 'act_planck_s08_s18_cmb_f150_night_map.fits')
-map090 = enmap.read_map(path + 'act_planck_s08_s18_cmb_f090_night_map.fits')
+def s18dStamp(ra, dec, data, name, width = 0.5, write = True):
+    #Find tile corresponding to RA, Dec
+    path = '/scratch/r/rbond/jorlo/S18d_202006/filteredMaps/'
+    tileName = tileFinder(ra, dec, data)
+    if tileName == None: return None
+    tile = enmap.read_map(path+tileName+'/Arnaud_M2e14_z0p4#'+tileName+'_filteredMap.fits')
+    
+    
+    stamp = reproject.postage_stamp(tile, ra, dec, width*60, 0.5)
+    if write:
+        stamp.wcs.wcs.crval = [ra,dec]
+        enmap.write_map('./for_tony/{}.fits'.format(name), stamp)
+    return stamp
 
-ignore = freqStamp(303.1103611788376,-56.83093699573238, map220, '220_ACT-CL_J2012.4-5649')
-ignore = freqStamp(303.1103611788376,-56.83093699573238, map150, '150_ACT-CL_J2012.4-5649')
-ignore = freqStamp(303.1103611788376,-56.83093699573238, map090, '090_ACT-CL_J2012.4-5649')
+with open('/scratch/r/rbond/jorlo/S18d_202006/selFn/tileDefinitions.yml') as f:
+    
+    s18d = yaml.load(f)
+"""
+mdcw_catalog = fits.open('/home/s/sievers/sdicker/ACTnCOWs/MADCOWSUnion.fits')
+
+ra = mdcw_catalog[1].data['RADeg']
+names = mdcw_catalog[1].data['name']
+dec = mdcw_catalog[1].data['decDeg']
+ra, dec = np.array(ra), np.array(dec)
+rich = mdcw_catalog[1].data['Rich']
+z = mdcw_catalog[1].data['Photz']
+
+flags = np.where((30<= rich) & (rich <=40))[0]
+
+ra, dec, names, rich = ra[flags], dec[flags], names[flags], rich[flags]
+
+lowest_y = []
+central_y = []
+
+for i in range(len(ra)):
+    stamp = s18dStamp(ra[i], dec[i], s18d, 'fakename', width = 0.5, write = False)
+    if stamp is None: 
+        lowest_y.append(0)
+        central_y.append(0)
+        continue
+    lowest_y.append(np.amin(stamp[0][29:31, 29:31]))
+    central_y.append(np.mean(stamp[0][29:31, 29:31]))
+
+n=10
+
+central_idx = np.argpartition(central_y, n)
+central_idx = central_idx[:n]
+lowest_idx = np.argpartition(lowest_y, n)
+lowest_idx = lowest_idx[:n]
+
+path = '/home/r/rbond/sigurdkn/project/actpol/map_coadd/20200228/release2/'
+
+
+maps = ['220', '150', '090']
+
+for i in central_idx:
+    for imap in maps:
+
+        cur_map = enmap.read_map(path + 'act_planck_s08_s18_cmb_f{}_night_map.fits'.format(imap))
+
+        #print(str(imap)+names[i].replace(' ', '_'))
+        stamp = freqStamp(ra[i], dec[i], cur_map, imap+names[i].replace(' ', '_'))
+        
+        print(i)
+        print(len(names))
+        print(len(ra))     
+        print(names[i])
+
+        fig = plt.figure()
+
+        plot = plt.imshow(stamp[0], extent = [-30, 30, -30, 30])
+        plt.scatter(0,0, marker = '+', color = 'r', alpha = 0.5)
+        plt.colorbar(plot, format='%.0e')
+        plt.title('Cluster {}, freq: {}GHz'.format(names[i], imap))
+        txt = plt.text(-25, 25, r'Central $y_0$ = {:0.3e}\n z: '.format(central_y[i], z[i]), size=11, color='black')
+        txt.set_path_effects([path_effects.withStroke(linewidth=5, foreground='w')])
+        plt.xlabel('Arcmin')
+        plt.ylabel('Arcmin')
+        plt.savefig('./plots/low_y/{}_central_{}.pdf'.format(imap, names[i].replace(' ', '_')))
+        plt.savefig('./plots/low_y/{}_central_{}.png'.format(imap, names[i].replace(' ', '_'), dpi = 300))
+        plt.show()
+        plt.close()
+"""
+#ignore = freqStamp(303.1103611788376,-56.83093699573238, map220, '220_ACT-CL_J2012.4-5649')
+#ignore = freqStamp(303.1103611788376,-56.83093699573238, map150, '150_ACT-CL_J2012.4-5649')
+#ignore = freqStamp(303.1103611788376,-56.83093699573238, map090, '090_ACT-CL_J2012.4-5649')
+maps = ['220', '150', '090']
+path = '/home/r/rbond/sigurdkn/project/actpol/map_coadd/20200228/release2/'
+
+dec, ra = -2.38, 33.92
+for imap in maps:
+
+    cur_map = enmap.read_map(path + 'act_planck_s08_s18_cmb_f{}_night_map.fits'.format(imap))
+
+
+    stamp = freqStamp(ra, dec, cur_map, 'ignore')
+
+    fig = plt.figure()
+
+    plot = plt.imshow(stamp[0], extent = [-30, 30, -30, 30])
+    plt.scatter(0,0, marker = '+', color = 'r', alpha = 0.5)
+    plt.colorbar(plot, format='%.0e')
+    plt.title('Test, freq: {}GHz'.format(imap))
+    plt.xlabel('Arcmin')
+    plt.ylabel('Arcmin')
+    plt.savefig('./plots/low_y/{}_central_test.pdf'.format(imap))
+    plt.savefig('./plots/low_y/{}_central_test.png'.format(imap, dpi = 300))
+    plt.show()
+    plt.close()
 
 #Stack on S18 cluster catalog
 
